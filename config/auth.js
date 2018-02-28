c0e6125689f9d96dc4357105d13842a08451751e
Ribhu Lahiri <ribhu197@gmail.com>
2018-02-27 21:14:22 -0800

    fixing facebook login

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '146869599328588', // your App ID
         'clientSecret'  : 'df17d188015dd6b91f3b4f73906b89e2', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL'   : 'https://a7-intraspect.herokuapp.com/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     }
