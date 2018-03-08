209bbe9cc12bf914c81b9cba1b9a7f7d3ba2c8c0
Prabhav Khandelwal <me@prabhavkhandelwal.com>
2018-03-07 17:44:51 -0800

    added name to score field

--- routes/homepage.js
+++ routes/homepage.js
@@ -9,7 +9,7 @@ exports.view = function(req, res){
 
 exports.addActivity = function(req,res){
 	mongoose.connect('mongodb://ribhu:pass1234@ds044907.mlab.com:44907/intraspect',function (err, database) {
-   		if (err) 
+   		if (err)
    			throw err
    		else
    		{
@@ -20,10 +20,12 @@ exports.addActivity = function(req,res){
                             console.log(req.body.newAct);
                             collection.update({'local.email':req.user.local.email},
                               {$push: {'local.activities' : newActivity}});
-                            //collection.find( {user.local : req.user.local.email}).push({})
+                            //collection.find( {user.local: req.user.local.email}).push({})
 			console.log('Connected to MongoDB');
+            console.log('Reason: ' + req.body.reason);
+            // console.log('Password: ' + req.body.password);
    		}
  	});
     	//var collection = db.collection('users');
-	//user.local.activities.push({name:newActivity});	
+	//user.local.activities.push({name:newActivity});
 }
